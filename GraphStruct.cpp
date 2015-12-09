6673950e7507c6a7f9e4587ab20daee5f8b74f56
Nestor Javier Hernandez Bautista <nestor.hernandez@definityfirst.com>
2015-12-09 16:05:34 -0600

    Change variable names from edgenode to Node, and from edgeNodes to GraphNodes

--- GraphStruct.cpp
+++ GraphStruct.cpp
@@ -1,22 +1,24 @@
 #include<iostream>
 using namespace std;
 
-/**************
-  GRAPH STRUCT
- *************
+/******************************************
+  GRAPH STRUCT: The structure is composed of 2 data structs
+
+
+ *****************************************
 */
 //Graph using adjency matrix
 #define MAXV 1000
 
 //Node Struct
-typedef struct edgenode_str{
+typedef struct Node_str{
   int y;
   int weight;
-  struct edgenode_str *next;
-}edgenode;
+  struct Node_str *next;
+}Node;
 
 typedef struct graph_str{
-  edgenode *edges[MAXV+1];  /*adjacency info*/
+  Node *graphNodes[MAXV+1];  /*adjacency info*/
   int degree[MAXV+1];       /**/
   int nvertices;            /*number of vertices in the graph*/
   int nedges;               /*number of edges in the graph*/
@@ -34,21 +36,21 @@ void initialize_graph(graph *g, bool directed){
   g->directed=directed;
 
   for(int i=1;i<=MAXV; i++)g->degree[i]=0;
-  for(int i=1;i<=MAXV; i++)g->edges[i]=NULL;
+  for(int i=1;i<=MAXV; i++)g->graphNodes[i]=NULL;
 
 
 }
 
 void insert_edge(graph *g, int x, int y, bool directed){
   //
-  edgenode *p;
-  p=(edgenode*)malloc(sizeof(edgenode));
+  Node *p;
+  p=(Node*)malloc(sizeof(Node));
 
   p->weight=0;
 
   p->y = y;
-  p->next=g->edges[x];
-  g->edges[x]=p;
+  p->next=g->graphNodes[x];
+  g->graphNodes[x]=p;
   g->degree[x]++;
 
   if(directed==false)
@@ -60,13 +62,13 @@ void insert_edge(graph *g, int x, int y, bool directed){
 
 void insert_edge_w(graph *g, int x, int y, bool directed,int wx, int wy){
   //
-  edgenode *p;
-  p=(edgenode*)malloc(sizeof(edgenode));
+  Node *p;
+  p=(Node*)malloc(sizeof(Node));
 
   p->weight=wy;
   p->y = y;
-  p->next=g->edges[x];
-  g->edges[x]=p;
+  p->next=g->graphNodes[x];
+  g->graphNodes[x]=p;
   g->degree[x]++;
 
   if(directed==false)
@@ -77,11 +79,11 @@ void insert_edge_w(graph *g, int x, int y, bool directed,int wx, int wy){
 
 
 void printGraph(graph *g){
-  edgenode *p;
+  Node *p;
   cout<<"flag\n";
   for(int i=1; i<=g->nvertices; i++){
     printf("%d: ",i);
-    p = g->edges[i];
+    p = g->graphNodes[i];
     while(p !=NULL){
       printf("%d(%d)->",p->y,p->weight);
       p=p->next;
